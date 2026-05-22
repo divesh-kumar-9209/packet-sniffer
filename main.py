55a9ed4a1018f4671caae5552d64258b8600cc46
Divesh Kumar <diveshkumar9209@outlook.com>
2026-05-22 23:07:53 +0530

    Version 3.5: Fixed some bugs

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import argparse
-from scapy.all import sniff, get_if_list
+from scapy.all import sniff, get_if_list, get_if_list, get_if_addr
 from sniffer import process_packet
 from stats import print_stats
 from config import DEFAULT_FILTER
 
 def list_interfaces():
-    print("\nAvailable Network Interfaces:")
-    for i, iface in enumerate(get_if_list()):
+    print("\nAvailable Network Interfaces:\n")
+
+    interfaces = get_if_list()
+
+    for i, iface in enumerate(interfaces):
+        try:
+            ip = get_if_addr(iface)
+        except:
+            ip = "No IP"
+
+        status = "ACTIVE" if ip != "0.0.0.0" else "INACTIVE"
+
         print(f"{i}: {iface}")
-    print()
+        print(f"   ↳ IP: {ip} | Status: {status}\n")
 
 def start_sniffer(interface, filter_rule):
     print(f"[*] Interface: {interface}")
@@ -22,6 +32,15 @@ def start_sniffer(interface, filter_rule):
         store=False
     )
 
+def get_interface_map():
+    interfaces = get_if_list()
+    mapping = {}
+
+    for i, iface in enumerate(interfaces):
+        mapping[str(i)] = iface
+
+    return mapping
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Packet Sniffer v3")
 
@@ -32,10 +51,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     try:
+        iface_map = get_interface_map()
         if args.list:
             list_interfaces()
         else:
-            start_sniffer(args.interface, args.filter)
+            selected_iface = args.interface
+
+        # Allow index instead of full NPF name  
+            iface_map = get_interface_map()
+            if selected_iface in iface_map:
+                selected_iface = iface_map[selected_iface]
+
+                start_sniffer(selected_iface, args.filter)
 
     except KeyboardInterrupt:
         print("\n[!] Sniffer stopped by user")
